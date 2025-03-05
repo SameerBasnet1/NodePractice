20005fe3081ca64af83f6dcb7297608595c39cca
SameerBasnet1 <sameerbasnet1071@gmail.com>
2025-03-05 16:43:24 +0545

    completed chap4 in chap3 file

--- 3/index.js
+++ 3/index.js
@@ -1,17 +1,13 @@
-// Title= NPM modules
-
+// Header Title= NPM modules
 // console.log('Testing');
+// const { format } = require('date-fns');
+// const { v4:uuid } = require('uuid');
 
-const { format } = require('date-fns');
-const { v4:uuid } = require('uuid');
+// console.log(format(new Date(),'yyyyMMdd\tHH:mm:ss'))
 
-console.log(format(new Date(),'yyyyMMdd\tHH:mm:ss'))
-
+// // console.log('Hello')
+// console.log(uuid())
 // console.log('Hello')
-
-console.log(uuid())
-
-console.log('Hello')
 
 
 //Major Minor Patch
@@ -19,3 +15,20 @@ console.log('Hello')
 // To change or Update  Just use ~ or ^
 // "~11.1.0" - 11.1.0 to 11.1.9
 
+
+// Event Emitter Index.js  for chap4
+// const logEvents = require('./logEvents');
+// const EventEmitter = require('events');
+
+// class MyEmitter extends EventEmitter {}
+
+// // Initialize object
+// const myEmitter = new MyEmitter();
+
+// // Add listener for the log event
+// myEmitter.on('log', (msg) => logEvents(msg));
+
+// setTimeout(() => {
+//     // Emit event
+//     myEmitter.emit('log', 'Log event emitted');
+// }, 2000);
